500b6ca866acd157b134af71d034f1fb23b766d8
chan <videoal@naver.com>
2019-11-30 17:04:58 +0900

    react init.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Subject from './components/Subject';
+import TOC from './components/TOC';
+import Content from './components/Content';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Subject title="Web" sub="world wide web"></Subject>
+      <TOC></TOC>
+      <Content title="html" desc="html is markup language."></Content>
     </div>
   );
 }
---
+++ src/components/TOC.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+function TOC() {
+    return (
+      <nav>
+          <li><a href="http://google.com">google</a></li>
+          <li><a href="http://naver.com">naver</a></li>
+      </nav>      
+    );
+  }
+  
+export default TOC;
